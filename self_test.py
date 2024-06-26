1ab670bf5db647e2e1d0cc633a054a5da45db77f
keith <1>
2024-06-26 14:02:50 +0800

    1

---
+++ self_test.py
@@ -0,0 +1,20 @@
+from servers.preprocessing_server import DataPreprocessor
+
+
+
+def test1():
+    pass
+
+
+def test2():
+    pass
+
+
+def test3():
+    pass
+
+
+if __name__ == "__main__":
+    # test1()
+    # test2()
+    test3()
