c249b490a5a396932e319a7cc499e3d15f122634
yuting <233333@qq.com>
2024-07-04 13:31:27 +0800

    1

--- self_test.py
+++ self_test.py
@@ -1,19 +1,85 @@
 from servers.preprocessing_server import DataPreprocessor
 
 
-
 def test1():
-    111
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\订单.xlsx',
+        f'订单.xlsx',
+        r'C:\Users\Admin\Desktop\process',
+        '订单'
+    )
+    file_preprocessor.preprocess()
 
 def test2():
-    pass
-
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\男鞋商品信息数据.xls',
+        f'男鞋商品信息数据.xls',
+        r'C:\Users\Admin\Desktop\process',
+        '男鞋商品信息数据'
+    )
+    file_preprocessor.preprocess()
 
 def test3():
-    pass
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\男鞋物价信息查询数据.xls',
+        f'男鞋物价信息查询数据.xls',
+        r'C:\Users\Admin\Desktop\process',
+        '男鞋物价信息查询数据'
+    )
+    file_preprocessor.preprocess()
+
+def test4():
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\女鞋商品信息数据.xls',
+        f'女鞋商品信息数据.xls',
+        r'C:\Users\Admin\Desktop\process',
+        '女鞋商品信息数据'
+    )
+    file_preprocessor.preprocess()
+
+def test5():
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\女鞋物价信息查询数据.xls',
+        f'女鞋物价信息查询数据.xls',
+        r'C:\Users\Admin\Desktop\process',
+        '女鞋物价信息查询数据'
+    )
+    file_preprocessor.preprocess()
+
+def test6():
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\商品库存.xlsx',
+        f'商品库存.xlsx',
+        r'C:\Users\Admin\Desktop\process',
+        '商品库存'
+    )
+    file_preprocessor.preprocess()
+
+def test7():
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\月聚水潭.xlsx',
+        f'月聚水潭.xlsx',
+        r'C:\Users\Admin\Desktop\process',
+        '月聚水潭'
+    )
+    file_preprocessor.preprocess()
+
+def test8():
+    file_preprocessor = DataPreprocessor(
+        r'C:\Users\Admin\Desktop\源表\月罗盘.xlsx',
+        f'月罗盘.xlsx',
+        r'C:\Users\Admin\Desktop\process',
+        '月罗盘'
+    )
+    file_preprocessor.preprocess()
 
 
 if __name__ == "__main__":
-    # test1()
-    # test2()
+    test1()
+    test2()
     test3()
+    test4()
+    # test5()   女鞋物价信息查询数据.xlsx
+    test6()
+    test7()
+    test8()
--- servers/preprocessing_server.py
+++ servers/preprocessing_server.py
@@ -1,9 +1,63 @@
 # 文件下载和预处理
-from .mylog_server import logger
-
-
+import os
+# from .mylog_server import logger
+import pandas as pd
+import xlrd
+from openpyxl.utils.exceptions import InvalidFileException
 
 class DataPreprocessor:
-    def __init__(self):
-        pass
+    def __init__(self, file_path, file_name, out_put_folder, file_text):
+        self.file_path = file_path
+        self.file_name = file_name
+        self.out_put_folder = out_put_folder
+        self.output_file_name = f"{file_text}.xlsx"
 
+    def preprocess(self):
+        self.copy_convert_excel()
+        file_ext = os.path.splitext(self.file_name)[1]
+        if file_ext == '.xls':
+            head_row = self.findHead_row()
+            self.delete_null_rows(head_row)b
+
+
+    def copy_convert_excel(self):
+        file_ext = os.path.splitext(self.file_name)[1]
+        print(file_ext)
+        xls_file = self.file_path
+        self.df = pd.read_excel(xls_file, sheet_name=None)  # 读取所有的 sheet
+        # 构建输出文件的完整路径
+        output_file_path = os.path.join(self.out_put_folder, self.output_file_name)
+        # 将数据写入 .xlsx 文件
+        with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
+            for sheet_name, sheet_df in self.df.items():
+                sheet_df.to_excel(writer, sheet_name='sheet1', index=False)
+        print(f'{xls_file} 转换为 {output_file_path} 完成。')
+        print(f"处理后的文件保存到：{output_file_path}")
+
+    def findHead_row(self):
+        output_file_path = os.path.join(self.out_put_folder, self.output_file_name)
+        df = pd.read_excel(output_file_path)
+        print(df.head())
+        count = 0
+        for index, row in df.iterrows():
+            print(index)
+            for item in row:
+                if pd.isnull(item) or item == '':
+                    pass
+                    # print(f'项 "{item}" 是空的')
+                else:
+                    count += 1
+                    if count == len(row):
+                        print(count)
+                        print(index)
+                        return index
+
+    def delete_null_rows(self, head_row):
+        output_file_path = os.path.join(self.out_put_folder, self.output_file_name)
+        df = pd.read_excel(output_file_path)
+        new_header = df.iloc[head_row]
+        head_row = head_row + 1
+        df = df[head_row:]
+        df.columns = new_header
+        df.to_excel(output_file_path, index=False)
+        print(df.head())
